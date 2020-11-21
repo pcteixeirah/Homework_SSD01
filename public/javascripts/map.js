8497a21a204642cf73caa897d818b6576e57499c
pcteixeirah <pcteixeirah@unal.edu.co>
2020-11-21 01:48:03 -0500

    erste Woche Zuordnung

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,16 +1,21 @@
-var map = L.map('main_map').setView([5.0676813,-75.499784],18);
+var map = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    /** 
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-    */
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+
 }).addTo(map);
 
-L.marker([5.0676813,-75.499784]).addTo(map);
-L.marker([5.0672805,-75.4990974]).addTo(map);
-L.marker([5.0669973,-75.5008676]).addTo(map);
+//L.marker([-34.6012424, -58.3861497]).addTo(map);
+//L.marker([-34.596932, -58.3881287]).addTo(map);
+//L.marker([-34.599564, -58.3778777]).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
